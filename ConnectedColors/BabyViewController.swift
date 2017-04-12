db039cbf694a22244b339f09edb5d13dd66c9c12
Rosa <Rosa@Mac-mini-van-Rosa.local>
2017-04-12 15:13:34 +0200

    First commit

---
+++ ConnectedColors/BabyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BabyViewController.swift
+//  ConnectedColors
+//
+//  Created by Rosa Meijers on 12-04-17.
+//  Copyright © 2017 Example. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class BabyViewController: UIViewController {
+    
+    
+    
+}
---
+++ ConnectedColors/ParentViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ParentViewController.swift
+//  ConnectedColors
+//
+//  Created by Rosa Meijers on 12-04-17.
+//  Copyright © 2017 Example. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class ParentViewController: UIViewController {
+    
+    @IBOutlet weak var codeTF: UITextField!
+    
+    
+    
+}
