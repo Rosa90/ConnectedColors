7d1fa5e49ad390e2943f254715dcf252e4ed4acd
Rosa <Rosa@Mac-mini-van-Rosa.local>
2017-04-12 16:11:06 +0200

    PairDevicesManager

--- ConnectedColors/ColorServiceManager.swift
+++ ConnectedColors/ColorServiceManager.swift
@@ -69,61 +69,61 @@ class ColorServiceManager : NSObject {
 
 }
 
-extension ColorServiceManager : MCNearbyServiceAdvertiserDelegate {
-
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
-        NSLog("%@", "didNotStartAdvertisingPeer: \(error)")
-    }
-
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
-        NSLog("%@", "didReceiveInvitationFromPeer \(peerID)")
-        invitationHandler(true, self.session)
-    }
-
-}
-
-extension ColorServiceManager : MCNearbyServiceBrowserDelegate {
-
-    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
-        NSLog("%@", "didNotStartBrowsingForPeers: \(error)")
-    }
-
-    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-        NSLog("%@", "foundPeer: \(peerID)")
-        NSLog("%@", "invitePeer: \(peerID)")
-        browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 10)
-    }
-
-    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
-        NSLog("%@", "lostPeer: \(peerID)")
-    }
-    
-}
-
-extension ColorServiceManager : MCSessionDelegate {
-
-    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        NSLog("%@", "peer \(peerID) didChangeState: \(state)")
-        self.delegate?.connectedDevicesChanged(manager: self, connectedDevices:
-            session.connectedPeers.map{$0.displayName})
-    }
-
-    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-        NSLog("%@", "didReceiveData: \(data)")
-        let str = String(data: data, encoding: .utf8)!
-        self.delegate?.colorChanged(manager: self, colorString: str)
-    }
-
-    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-        NSLog("%@", "didReceiveStream")
-    }
-
-    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-        NSLog("%@", "didStartReceivingResourceWithName")
-    }
-
-    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
-        NSLog("%@", "didFinishReceivingResourceWithName")
-    }
-
-}
+//extension ColorServiceManager : MCNearbyServiceAdvertiserDelegate {
+//
+//    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+//        NSLog("%@", "didNotStartAdvertisingPeer: \(error)")
+//    }
+//
+//    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+//        NSLog("%@", "didReceiveInvitationFromPeer \(peerID)")
+//        invitationHandler(true, self.session)
+//    }
+//
+//}
+//
+//extension ColorServiceManager : MCNearbyServiceBrowserDelegate {
+//
+//    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+//        NSLog("%@", "didNotStartBrowsingForPeers: \(error)")
+//    }
+//
+//    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+//        NSLog("%@", "foundPeer: \(peerID)")
+//        NSLog("%@", "invitePeer: \(peerID)")
+//        browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 10)
+//    }
+//
+//    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+//        NSLog("%@", "lostPeer: \(peerID)")
+//    }
+//    
+//}
+//
+//extension ColorServiceManager : MCSessionDelegate {
+//
+//    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+//        NSLog("%@", "peer \(peerID) didChangeState: \(state)")
+//        self.delegate?.connectedDevicesChanged(manager: self, connectedDevices:
+//            session.connectedPeers.map{$0.displayName})
+//    }
+//
+//    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+//        NSLog("%@", "didReceiveData: \(data)")
+//        let str = String(data: data, encoding: .utf8)!
+//        self.delegate?.colorChanged(manager: self, colorString: str)
+//    }
+//
+//    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+//        NSLog("%@", "didReceiveStream")
+//    }
+//
+//    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+//        NSLog("%@", "didStartReceivingResourceWithName")
+//    }
+//
+//    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
+//        NSLog("%@", "didFinishReceivingResourceWithName")
+//    }
+//
+//}
